8cab2692c6092d38fc9e7f97c0d7ee77737cb21c
Mahdi Harrabi <harrabi.mahdi88@gmail.com>
2025-05-27 03:40:57 +0300

    Reapply " Add Matching and Messaging services with Docker and configuration"
    This reverts commit c2e2804fb201d2441041d93b2e32396c743d8713.

---
+++ server/model/src/main/java/Message.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private String id;
+    private String content;
+    private LocalDateTime timestamp;
+    private Group group;
+
+    public Message() {}
+
+    public Message(String id, String content, LocalDateTime timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public void deleteMessage() {
+        // TODO: Implement deletion logic
+    }
+
+    public void editMessage(String newContent) {
+        // TODO: Implement edit logic
+        this.content = newContent;
+    }
+}
